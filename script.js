135466e99d367b430875a98e672b57dcba0a5ae4
mountt96 <159955699+mountt96@users.noreply.github.com>
2024-02-23 13:52:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", function() {
+    console.log("Button clicked!"); // Add this line for debugging
+    // Redirect to another page
+    window.location.href = "SecureSignIn.html";
+});
